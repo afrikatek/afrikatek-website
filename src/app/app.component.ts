534557ae63f3c1e1bd58d66357eb9656ca059702
Murukai Gumbo-Mberi <murukai.mberi@afrikatek.co.za>
2025-05-01 19:54:30 +0200

    main - home navbar and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./layouts/nav-bar/nav-bar.component";
+import { FooterComponent } from "./layouts/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
