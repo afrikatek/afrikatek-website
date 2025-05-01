7dff844c41f0d1092c9eb572b6761b7b13dad2ab
Murukai Gumbo-Mberi <murukai.mberi@afrikatek.co.za>
2025-05-01 20:34:47 +0200

    main - other pages of the website

--- src/app/layouts/footer/footer.component.ts
+++ src/app/layouts/footer/footer.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
+import {faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faInstagram} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faLinkedin = faLinkedin;
+  faInstagram = faInstagram;
+  faYouTube = faYoutube;
 }
