390af183e1e5f2ceef23a8e4b8f0f6c9a9522df8
Murukai Gumbo-Mberi <murukai.mberi@afrikatek.co.za>
2025-05-02 16:15:33 +0200

    main - contact us page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from './main/home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {ContactComponent} from './contact/contact.component';
+import {ServicesComponent} from './services/services.component';
+import {ListComponent} from './blog/list/list.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'products', component: ProjectsComponent},
+  {path: 'services', component: ServicesComponent},
+  {path: 'blog', component: ListComponent}
 ];
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {faBuilding, faEnvelope, faPhoneAlt,} from '@fortawesome/free-solid-svg-icons';
+import {FaIconComponent} from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [
+    FaIconComponent
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
-
+  faBuilding = faBuilding;
+  faPhone = faPhoneAlt;
+  faEmail = faEnvelope;
+  protected readonly faEnvelope = faEnvelope;
 }
